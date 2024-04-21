3edf180e5c4d6c8ab595d0a9791feffdd1a145d4
fedebrizu <165932058+fedebrizu@users.noreply.github.com>
2024-04-21 20:23:23 -0300

    Add files via upload

--- Guia-4.hs
+++ Guia-4.hs
@@ -44,7 +44,7 @@ cantDigitos n | n < 10 = 1
 
 
 iesimoDigito :: Int -> Int -> Int
-iesimoDigito n m | m < cantDigitos(n) = iesimoDigito(div n 10) m 
+iesimoDigito n m | m < cantDigitos n = iesimoDigito(div n 10) m 
                  | otherwise = mod n 10
 -------------
 
@@ -58,7 +58,6 @@ comparacionDigitos :: Int -> Int -> Bool
 comparacionDigitos n pos | pos == 1 && mod n 10 == div n 10 = True
                          | iesimoDigito n pos == iesimoDigito n (cantDigitos (n) - pos + 1) = True --evalúa por izquierda y por derecha
                          | otherwise = False
-
 -------------
 
 --  Ej 10  --
@@ -88,6 +87,7 @@ factorial n | n == 0 = 1
 eAprox :: Int -> Float
 eAprox n | n == 0 = 1
          | otherwise = 1 / (fromIntegral (factorial (n))) + eAprox (n-1) -- fromIntegral es una función ya definida de Haskell
+-------------
 
 --  Ej 12  --
 sucesionRaizDe2 :: Int -> Float
@@ -118,7 +118,7 @@ combinacionesDeSuma :: Int -> Int -> Int
 combinacionesDeSuma n m | n == 1 = sumaAB n m
                         | otherwise = sumaAB n m + combinacionesDeSuma (n-1) m
 
-sumaAB :: Int -> Int -> Int
+sumaAB :: Int -> Int -> Int 
 sumaAB a b | b == 1 = a + b
            | otherwise = a + b + sumaAB a (b-1)
 -------------
@@ -130,18 +130,98 @@ sumaRacionales n m | n == 1 = sumaRacInterna n m
 
 sumaRacInterna :: Int -> Int -> Float
 sumaRacInterna p q | q == 1 = fromIntegral p / fromIntegral q
-                    | otherwise = fromIntegral p / fromIntegral q + sumaRacInterna p (q-1)
+                   | otherwise = fromIntegral p / fromIntegral q + sumaRacInterna p (q-1)
 -------------
 
 --  Ej 16  --
 -- a)
-menorDivisor :: Int -> Int
-menorDivisor n = buscaDivisorDesde n 2
+menorDivisor :: Int -> Int --sin contar el 1
+menorDivisor n = primerDivisorDesde n 2
 
-buscaDivisorDesde :: Int -> Int -> Int
-buscaDivisorDesde n k | k == n = n
-                      | mod n k == 0 = k
-                      | otherwise = buscaDivisorDesde n (k+1)
+primerDivisorDesde :: Int -> Int -> Int -- devuelve el primer divisor que encuentre de n, desde k (con k<=n)
+primerDivisorDesde n k | k == n = n
+                       | mod n k == 0 = k
+                       | otherwise = primerDivisorDesde n (k+1)
 
 -- b)
---esPrimo :: Int -> Bool
+esPrimo :: Int -> Bool
+esPrimo n = menorDivisor n == n
+
+-- c)
+sonCoprimos :: Int -> Int -> Bool
+sonCoprimos n m = comparaDivisores n m 2
+
+comparaDivisores :: Int -> Int -> Int -> Bool
+comparaDivisores n m k | primerDivisorDesde n k == primerDivisorDesde m k = False 
+                       | k >= n || k >= m = True
+                       | otherwise = comparaDivisores n m (k+1)
+
+-- d)
+nEsimoPrimo :: Int -> Int
+nEsimoPrimo n = cuentaPrimos n 3 1
+
+cuentaPrimos :: Int -> Int -> Int -> Int --cuenta la cantidad de numeros primos (n), empezando desde k
+cuentaPrimos n k j | j == n = k-1
+                   | esPrimo k == True = cuentaPrimos n (k+1) (j+1) --j cuenta los numeros primo registrados
+                   | otherwise = cuentaPrimos n (k+1) j --hace recursión sobre k nada más
+-------------
+
+--  Ej  17  --
+esFibonacci :: Int -> Bool
+esFibonacci n = recorreFibonacci n 1
+
+recorreFibonacci :: Int -> Int -> Bool --detecta si el numero q recibe pertenece al fibonacci de algún k
+recorreFibonacci n k | fibonacci k > n = False
+                     | fibonacci k == n = True
+                     | otherwise = recorreFibonacci n (k+1)
+-------------
+
+--  Ej 18  --
+mayorDigitoPar :: Int -> Int
+mayorDigitoPar n = deteccionMayorPar n (cantDigitos n) (-1)
+
+deteccionMayorPar :: Int -> Int -> Int -> Int
+deteccionMayorPar n k p | k == 0 = p
+                        | mod (iesimoDigito n k) 2 == 0 && iesimoDigito n k > p = deteccionMayorPar n (k-1) (iesimoDigito n k)
+                        | otherwise = deteccionMayorPar n (k-1) p
+-------------
+
+--  Ej 19  --
+esSumaInicialDePrimos :: Int -> Bool
+esSumaInicialDePrimos n = recorrePrimos n 1 0
+
+recorrePrimos :: Int -> Int -> Int -> Bool
+recorrePrimos n k s | n < 2 = False
+                    | n < s = False
+                    | n == s = True
+                    | otherwise = recorrePrimos n (k+1) (s + nEsimoPrimo k)
+-------------
+
+--  Ej 20  --
+tomaValorMax :: Int -> Int -> Int 
+tomaValorMax n1 n2 = sumaDivisores n1 (n2 - n1) 1
+
+sumaDivisores :: Int -> Int -> Int -> Int
+sumaDivisores m k i | k == 0 = m
+                    | todosLosDivisores m 1 > todosLosDivisores (m + i) 1 = sumaDivisores m (k-1) (i+1)
+                    | todosLosDivisores m 1 < todosLosDivisores (m + i) 1 = sumaDivisores (m + i) (k-1) 1
+
+todosLosDivisores :: Int -> Int -> Int --devuelve la suma de todos los divisores de n, empezando en k
+todosLosDivisores n k | k == n = k
+                      | mod n k == 0 = k + todosLosDivisores n (k+1)
+                      | otherwise = todosLosDivisores n (k+1)
+-------------
+
+--  Ej 21  --
+pitagoras :: Int -> Int -> Int -> Int
+pitagoras m n r = totalParesPQ m n r 0
+
+totalParesPQ :: Int -> Int -> Int -> Int -> Int
+totalParesPQ p q r i | p < 0 = i
+                     | otherwise = totalParesPQ (p-1) q r (i + totalParesInterna p q r 0)
+
+totalParesInterna :: Int -> Int -> Int -> Int -> Int
+totalParesInterna p q r j | q < 0 = j
+                          | p^2 + q^2 <= r^2 = totalParesInterna p (q-1) r (j+1)
+                          | otherwise = totalParesInterna p (q-1) r j
+-------------
